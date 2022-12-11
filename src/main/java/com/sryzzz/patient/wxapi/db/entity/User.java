f36abdb49c96ef9a95ee104504a11f90947fe6b8
sryzzz <1299975117@qq.com>
2022-12-11 23:38:53 +0800

    feat: 更新实体类字段说明

--- src/main/java/com/sryzzz/patient/wxapi/db/entity/User.java
+++ src/main/java/com/sryzzz/patient/wxapi/db/entity/User.java
@@ -1,17 +1,53 @@
 package com.sryzzz.patient.wxapi.db.entity;
 
 import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
 
 /**
+ * <p>
+ * 患者小程序账号
+ * </p>
+ *
  * @author sryzzz
  */
 @Data
+@ToString
 public class User {
+
+    /**
+     * 主键
+     */
     private Integer id;
+
+    /**
+     * 唯一授权字符串
+     */
     private String openId;
+
+    /**
+     * 微信昵称
+     */
     private String nickname;
+
+    /**
+     * 微信头像
+     */
     private String photo;
+
+    /**
+     * 性别
+     */
     private String sex;
+
+    /**
+     * 状态：1代表正常，2代表禁用
+     */
     private byte status;
-    private String createTime;
+
+    /**
+     * 创建日期
+     */
+    private Date createTime;
 }
--- src/main/java/com/sryzzz/patient/wxapi/db/entity/UserInfoCard.java
+++ src/main/java/com/sryzzz/patient/wxapi/db/entity/UserInfoCard.java
@@ -1,21 +1,71 @@
 package com.sryzzz.patient.wxapi.db.entity;
 
 import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
 
 /**
+ * 患者信息卡
+ *
  * @author sryzzz
  */
 @Data
+@ToString
 public class UserInfoCard {
+
+    /**
+     * 主键
+     */
     private Integer id;
+
+    /**
+     * 患者ID
+     */
     private Integer userId;
+
+    /**
+     * UUID，当做患者信息卡的编号
+     */
     private String uuid;
+
+    /**
+     * 姓名
+     */
     private String name;
+
+    /**
+     * 性别
+     */
     private String sex;
+
+    /**
+     * 身份证号
+     */
     private String pid;
+
+    /**
+     * 手机号码
+     */
     private String tel;
-    private String birthday;
+
+    /**
+     * 出生日期
+     */
+    private Date birthday;
+
+    /**
+     * 疾病史，JSON 数组
+     */
     private String medicalHistory;
+
+    /**
+     * 保险类型
+     */
     private String insuranceType;
+
+    /**
+     * 是否录入面部信息
+     */
     private Boolean existFaceModel;
 }
